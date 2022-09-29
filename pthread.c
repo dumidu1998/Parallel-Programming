b5fd0b66d6ca1482d24378496ed6348f0138dbd0
Dumidu Kasun Bandara Rajakaruna <50117273+dumidu1998@users.noreply.github.com>
2022-09-29 15:50:18 +0530

    ss

--- pthread.c
+++ pthread.c
@@ -6,7 +6,7 @@
 #include <sys/time.h>
 
 #define RANDLIMIT 5 
-#define N 5000     
+#define N 2000     
 #define NUMLIMIT 70.0
 #define THREADS 8
 
--- seq.c
+++ seq.c
@@ -4,7 +4,7 @@
 #include <sys/time.h>
 
 #define RANDLIMIT 5
-#define N 5000
+#define N 2000
 #define NUMLIMIT 70.0
 
 float a[N][N];
